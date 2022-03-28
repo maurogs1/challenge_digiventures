0ce219cd5ccbd11bd55a5a15b7e2c3f1ad5624f3
maurogs1 <maurosaravia59@gmail.com>
2022-03-28 08:47:35 -0300

    test port

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ const corsOptions = {
 };
 
 
-const port = parseInt(process.env.PORT, 10) || 3000;
+const port = process.env.PORT || 3000;
+
 const dev = process.env.NODE_ENV !== "production";
+
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
