8f86a0adda763b842cd934d7df72c173f04425dc
Mauro <maurosaravia59@gmail.com>
2022-03-29 00:09:15 -0300

    url to deploy

--- front/components/Presentation.js
+++ front/components/Presentation.js
@@ -1,9 +1,35 @@
 export const Presentation = () => {
-    return (
-        <div className="d-flex justify-content-center align-items-center w-100 vh-100">
-        
-            <h2>Challenge de digiventures</h2>
+  return (
+    <div className="d-flex justify-content-center align-items-center w-100 vh-100 flex-column">
+      <h2>
+        Challenge de{" "}
+        <span>
+          <a href="https://www.digiventures.la/" target="_blank">
+            Digiventures
+          </a>
+        </span>
+      </h2>
+      <h3></h3>
 
-        </div>
-    )
-}
+      <p>
+        <a
+          className="m-1"
+          href="https://github.com/DigiventuresFintech/home-interview-challenge"
+          target="_blank"
+        >
+          Acceder al github
+        </a>
+      </p>
+      <p>
+        <a
+          className="m-1"
+          href="https://github.com/maurogs1/challenge_digiventures"
+          target="_blank"
+        >
+          Github
+        </a>
+        del código fuente de la solución
+      </p>
+    </div>
+  );
+};
--- pages/[path].js
+++ pages/[path].js
@@ -37,7 +37,7 @@ export default function Page({ props}) {
 
 Page.getInitialProps = async (ctx) => {
   try {
-    const res = await fetch(`http://localhost:3000/configuration/${ctx.query.path}`);
+    const res = await fetch(`https://digiventures-challenge.herokuapp.com/configuration/${ctx.query.path}`);
     const json = await res.json()
     return { props: json }  
   } catch (error) {
